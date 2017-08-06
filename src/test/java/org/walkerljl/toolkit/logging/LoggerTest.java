acdc8bd1cc7d3e0f2f1f5e54d3ef3aa3452826da
walkerljl <walkerljlx@163.com>
2017-08-06 12:02:50 +0800

    Modified

--- src/test/java/org/walkerljl/toolkit/logging/LoggerTest.java
+++ src/test/java/org/walkerljl/toolkit/logging/LoggerTest.java
@@ -2,11 +2,6 @@ package org.walkerljl.toolkit.logging;
 
 import org.junit.Test;
 
-/**
- *
- * @author junlin.ljl
- * @version $Id: LoggerTest.java, v 0.1 2017年06月18日 上午9:54 junlin.ljl Exp $
- */
 public class LoggerTest {
 
     @Test
